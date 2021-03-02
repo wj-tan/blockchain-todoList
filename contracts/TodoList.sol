16f3dfe0221ff39aec6142143d6136591217faab
Tan Wei Jian <1901864@sit.singaporetech.edu.sg>
2021-03-02 21:46:17 +0800

    Created struct for task and a method to put task into to do list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -7,4 +7,18 @@ contract TodoList {
     // State variable are written to the blockchain, they represent the state of the smart contract in the blockchain
     // State of the smart contract changes whenever the taskCount changes
     uint public taskCount = 0;
+
+    struct Task {
+        uint id; //id of the task
+        string content; //task content
+        bool completed; //checkbox for whether task is completed
+    }
+
+    mapping(uint => Task) public tasks;
+
+    // Function to put task into the to do list
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
